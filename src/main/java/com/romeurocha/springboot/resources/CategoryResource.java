6795b2573a58c73e331a841c6fb790342865da2c
RomeuRocha <romeu.d.rocha@gmail.com>
2020-07-16 15:53:08 -0300

    criando repository e service para category

--- src/main/java/com/romeurocha/springboot/resources/CategoryResource.java
+++ src/main/java/com/romeurocha/springboot/resources/CategoryResource.java
@@ -1,16 +1,26 @@
 package com.romeurocha.springboot.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.romeurocha.springboot.domain.Category;
+import com.romeurocha.springboot.services.CategoryService;
+
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-
-	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "REST ESTÁ FUNCIONANDO";
+	
+	@Autowired
+	private CategoryService service;
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	public ResponseEntity<Category> find(@PathVariable Integer id) {
+		Category obj = service.findById(id);
+		return ResponseEntity.ok(obj);
 	}
 	
 }
---
+++ src/main/java/com/romeurocha/springboot/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.romeurocha.springboot.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.romeurocha.springboot.domain.Category;
+import com.romeurocha.springboot.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public Category findById(Integer id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+
+}
