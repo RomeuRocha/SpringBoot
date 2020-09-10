de6d55561ff507dd554759428a60f985e7267acc
RomeuRocha <romeu.d.rocha@gmail.com>
2020-09-10 16:28:17 -0300

    post category

--- src/main/java/com/romeurocha/springboot/resources/CategoryResource.java
+++ src/main/java/com/romeurocha/springboot/resources/CategoryResource.java
@@ -1,11 +1,15 @@
 package com.romeurocha.springboot.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.romeurocha.springboot.domain.Category;
 import com.romeurocha.springboot.services.CategoryService;
@@ -21,6 +25,14 @@ public class CategoryResource {
 	public ResponseEntity<Category> find(@PathVariable Integer id) {
 		Category obj = service.findById(id);
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();//retornar a URI do objeto inserido
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
